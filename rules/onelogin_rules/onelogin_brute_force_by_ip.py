6c384c75bfc72553884479b85d55057f1d08bdd8
JPhenglavong <johnny.phenglavong@panther.io>
2023-08-16 12:25:51 +0700

    Update onelogin_brute_force_by_ip.py

--- rules/onelogin_rules/onelogin_brute_force_by_ip.py
+++ rules/onelogin_rules/onelogin_brute_force_by_ip.py
@@ -1,6 +1,6 @@
 def rule(event):
     # filter events; event type 6 is a failed authentication
-    return event.get("event_type_id") == 6
+    return event.get("event_type_id") == "6"
 
 
 def title(event):
