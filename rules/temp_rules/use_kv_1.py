ac8c1ed4d3e2efcee93b30becbce257390ccc851
maxrichie5 <maxrichmond@panther.com>
2023-10-12 12:10:29 -0600

    updates

--- rules/temp_rules/use_kv_1.py
+++ rules/temp_rules/use_kv_1.py
@@ -1,11 +1,15 @@
 from panther_detection_helpers.caching import put_string_set, get_string_set
-import time
+import random
 
 
 def rule(event):
+    r = random.randint(1, 5)
     key = event.get("eventType") or event.get("action") or "default"
-    val = event.get("eventID") or event.get("interfaceId") or "default"
-    t = time.mktime(time.strptime(event.get("p_event_time", "").split(".")[0], "%Y-%m-%d %H:%M:%S"))
-    put_string_set(key, [val], epoch_seconds=int(t) + 1_000)
-    get_string_set(key, force_ttl_check=True)
+
+    if r == 5:
+        val = event.get("eventID") or event.get("interfaceId") or "default"
+        put_string_set(key, [val], epoch_seconds=event.event_time_epoch() + 1_000)
+    else:
+        get_string_set(key, force_ttl_check=True)
+
     return False
